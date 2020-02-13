d167015e286a09e23108a7fa3435a384e7bb450b
Pedro Santos <pedrosantos@frwk.com.br>
2020-02-13 17:11:58 -0300

    Fluttertube - Finalizado

--- fluttertube/lib/main.dart
+++ fluttertube/lib/main.dart
@@ -1,9 +1,27 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
+        child: MaterialApp(
+          title: 'FlutterTube',
+          debugShowCheckedModeBanner: false,
+          home: Home(),
+        ),
+      ),
+    );
+  }
+}
+
