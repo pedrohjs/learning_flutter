aeff527b3040566c67c2e9d9a154ab4f6d9396c1
Pedro Santos <pedro_ssantoss@yahoo.com.br>
2020-02-12 22:03:22 -0300

    Fluttertube - Finalizando Sistema de Buscas

--- fluttertube/lib/screens/home.dart
+++ fluttertube/lib/screens/home.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertube/delegates/data_search.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset('images/yt_logo_rgb_dark.png'),
+        ),
+        backgroundColor: Colors.black87,
+        elevation: 0,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: (){
+
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              String result = await showSearch(context: context, delegate: DataSearch());
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
